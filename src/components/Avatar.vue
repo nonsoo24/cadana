81f846f61e8216279589bc5483a43c16adf5bd2e
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-03-16 03:05:42 +0100

    feat: create reusuable avatar component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="[wrapperClass, 'rounded-full']">
+    <figure>
+      <img :src="image" :alt="alt" :class="imgClass" />
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Avatar",
+  props: {
+    image: String,
+    alt: String,
+    imgClass: String,
+    wrapperClass: String,
+  },
+};
+</script>
