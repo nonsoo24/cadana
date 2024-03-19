b72660b110fa3f2a20a88eb3a76cefb4d5b823b7
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-03-19 23:57:20 +0100

    chore: update imnvoice and dashboard screens

---
+++ src/pages/AppDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="flex justify-center items-center mt-60">
+    <div class="w-[400px] h-[400px]">
+      <p class="text-textColor-1 font-bold text-size1">
+        {{ $route.meta.pageTitle }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AppDashboard",
+};
+</script>
