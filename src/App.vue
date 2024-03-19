6127333124af58845d039f73cb0febba147a1062
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-03-19 13:58:12 +0100

    chore: implement app layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <router-view />
+  <main>
+    <component :is="$route.meta.layout || 'div'">
+      <router-view />
+    </component>
+  </main>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -22,6 +20,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
