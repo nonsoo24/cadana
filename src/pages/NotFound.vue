f942864ebdb1a4bf1bc604d88a1f5e098ac2cce7
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-03-20 00:00:43 +0100

    feat: implement NotFound page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>Oops, it looks like the page you're looking for doesn't exist.</h1>
+    <section class="flex justify-center items-center mt-60">
+      <div class="w-[400px] h-[400px]">
+        <p class="text-textColor-1 font-bold text-size1">
+          Oops, it looks like the page you're looking for doesn't exist
+        </p>
+      </div>
+    </section>
   </template>
 
 <script>
