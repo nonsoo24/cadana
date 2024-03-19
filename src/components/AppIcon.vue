1f2c744ff2d6389ec8dd0be16516f38d8712b2f2
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-03-19 13:58:33 +0100

    chore: implement pages

---
+++ src/components/AppIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <component
+    :is="iconComponent"
+    role="img"
+    class="inline-block fill-current"
+    style="height: 1em; width: 1em;"
+  />
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue';
+
+const icons = {
+  dropdown: '../assets/icons/dropdown.svg',
+  moon: '../assets/icons/moon.svg',
+  wallets: '../assets/icons/wallets.svg',
+  logo: '../assets/cadana-logo.svg',
+  ArrowDown: '../assets/icons/arrow-down.svg',
+  ArrowUp: '../assets/icons/arrow-up.svg',
+};
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const iconComponent = computed(() => icons[props.name]);
+</script>
---
+++ src/components/HeaderTitle.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h3 :class="titleClass">{{ title }}</h3>
+    <p :class="subtitleClass">{{ subtitle }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, watchEffect } from 'vue';
+
+export default {
+  name: 'HeaderTitle',
+  props: {
+    title: String,
+    subtitle: {
+      type: String,
+      default: "",
+    },
+    titleClass: {
+      type: String,
+      default: "text-size4 text-textColor-2",
+    },
+    subtitleClass: {
+      type: String,
+      default: "text-size2 text-textColor-1",
+    },
+  },
+  setup(props) {
+    const subtitleStyle = ref('');
+    const titleStyle = ref('');
+
+    watchEffect(() => {
+      titleStyle.value = props.titleStyle || '';
+      subtitleStyle.value = props.subtitleStyle || '';
+    });
+
+    return {
+      titleStyle,
+      subtitleStyle,
+    };
+  },
+};
+</script>
