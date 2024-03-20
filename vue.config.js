f86da5b593988dc499884ee252da7c13d187e6c2
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-03-20 23:29:46 +0100

    chore: modify vue config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  // chainWebpack: (config) => {
-  //   const svgRule = config.module.rule('svg');
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
 
-  //   svgRule.uses.clear();
-  //   svgRule
-  //     .use('vue-svg-loader')
-  //     .loader('vue-svg-loader');
-  // },
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
   transpileDependencies: true,
 });
