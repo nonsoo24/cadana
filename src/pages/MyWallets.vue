c2fd2044c83bad282fa5248820bc490af1fe731a
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-03-20 23:27:14 +0100

    style: update dark theme styles on wallet screen

--- src/pages/MyWallets.vue
+++ src/pages/MyWallets.vue
@@ -1,92 +1,94 @@
 <template>
   <section class="wrapper">
-    <section class="dark:bg-dark-1  bg-grey-2 py-9">
+    <section class="dark:bg-dark-1 bg-grey-2 py-9">
       <p class="font-semibold text-size1 text-textColor-1 dark:text-white">
         My Wallets
       </p>
 
-      <div class=" mt-8 flex justify-center">
-       <div>
-        <div class="relative">
-          <CreditCard
-            cardOwner="Cadana"
-            bankName="Universal Bank"
-            cardNumber="5495  7381  3759  2321"
-            expiryDate="04/24"
-            cardType="mastercard"
-            cardWrapper="h-[210px] w-[354px]"
-          />
-          <div class="absolute top-40 left-4">
+      <div class="mt-8 flex justify-center">
+        <div>
+          <div class="relative">
             <CreditCard
               cardOwner="Cadana"
-              bankName="Commercial Bank"
-              cardNumber="85952548****"
-              expiryDate="09/25"
-              :cardTypeLogo="VisaLogo"
-              cardType="visa"
-              cardWrapper="w-[324px] h-[172px]"
-              cardIconClass="!mt-4"
-              cardNumberClass="!mt-2"
-              cardExpiryClass="!mt-1"
+              bankName="Universal Bank"
+              cardNumber="5495  7381  3759  2321"
+              expiryDate="04/24"
+              cardType="mastercard"
+              cardWrapper="h-[210px] w-[354px]"
             />
-          </div>
-        </div>
-
-        <div
-          class="bg-grey-1 w-[373px] h-[181px] p-6 mb-5 rounded-lg mt-40 border border-grey-3 dark:bg-dark-3 dark:border-dark-3"
-        >
-          <div class="grid grid-cols-2 gap-5">
-            <div>
-              <p
-                class="text-textColor-2 text-size4 font-medium text-left dark:textColor-3"
-              >
-                Your Balance
-              </p>
-              <AppAmount
-                :amount="accountSummary.balance"
-                :currencyCode="CURRENCY_CODES.USD"
+            <div class="absolute top-40 left-4">
+              <CreditCard
+                cardOwner="Cadana"
+                bankName="Commercial Bank"
+                cardNumber="85952548****"
+                expiryDate="09/25"
+                :cardTypeLogo="isDark ? VisaLogoLight : VisaLogo"
+                cardType="visa"
+                cardWrapper="w-[324px] h-[172px]"
+                cardIconClass="!mt-4"
+                cardNumberClass="!mt-2"
+                cardExpiryClass="!mt-1"
               />
             </div>
-            <div class="flex gap-4 items-end">
-              <AppIcon name="arrow-up" />
-              <p class="font-medium text-size4 text-success">
-                {{ accountSummary.percentageGain }}
-              </p>
-              <AppIcon name="arrow-down" />
-              <p class="font-medium text-size4 text-error">
-                {{ accountSummary.percentageLoss }}
-              </p>
+          </div>
+
+          <div
+            class="bg-grey-1 w-[373px] p-6 mb-5 rounded-lg mt-40 border border-grey-3 dark:bg-dark-3 dark:border-dark-3"
+          >
+            <div class="grid grid-cols-2 gap-5">
+              <div>
+                <p
+                  class="text-textColor-2 text-size4 font-medium text-left dark:textColor-3"
+                >
+                  Your Balance
+                </p>
+                <AppAmount
+                  :amount="accountSummary.balance"
+                  :currencyCode="CURRENCY_CODES.USD"
+                />
+              </div>
+              <div class="flex gap-4 items-end">
+                <AppIcon name="arrow-up" />
+                <p class="font-medium text-size4 text-success">
+                  {{ accountSummary.percentageGain }}
+                </p>
+                <AppIcon name="arrow-down" />
+                <p class="font-medium text-size4 text-error">
+                  {{ accountSummary.percentageLoss }}
+                </p>
+              </div>
+            </div>
+            <div class="flex justify-center my-5 mx-5">
+              <div class="border-b border-b-grey-3 w-[289px]" />
+            </div>
+            <div class="grid grid-cols-2 gap-5">
+              <AppTitle
+                title="Currency"
+                subtitle="USD / US Dollar"
+                titleClass="text-textColor-2 text-size4 font-medium text-left dark:textColor-3"
+                subtitleClass="font-semibold text-size4 text-textColor-1 text-left dark:text-white"
+              />
+              <AppTitle
+                title="Status"
+                subtitle="Active"
+                titleClass="font-medium text-size4 text-textColor-2 text-left"
+                subtitleClass="font-semibold text-size4 text-textColor-1 text-left dark:text-white"
+              />
             </div>
           </div>
-          <div class="flex justify-center my-5 mx-5">
-            <div class="border-b border-b-grey-3 w-[289px]" />
-          </div>
-          <div class="grid grid-cols-2 gap-5">
-            <AppTitle
-              title="Currency"
-              subtitle="USD / US Dollar"
-              titleClass="text-textColor-2 text-size4 font-medium text-left dark:textColor-3"
-              subtitleClass="font-semibold text-size4 text-textColor-1 text-left dark:text-white"
-            />
-            <AppTitle
-              title="Status"
-              subtitle="Active"
-              titleClass="font-medium text-size4 text-textColor-2 text-left"
-              subtitleClass="font-semibold text-size4 text-textColor-1 text-left dark:text-white"
-            />
-          </div>
+          <AppButton
+            label="Add New Card"
+            type="button"
+            :icon="isDark ? PlusSecondary : PlusPrimary"
+            width="w-[354px]"
+            variant="secondary"
+            buttonClass="rounded-lg mx-5 my-3  !dark:bg-dark-2 !dark:text-secondary"
+          />
         </div>
-        <AppButton
-          label="Add New Card"
-          width="w-[354px]"
-          variant="secondary"
-          buttonClass="rounded-lg mx-5 my-3"
-        />
-       </div>
       </div>
     </section>
 
-    <section class="dark:bg-dark-2  bg-white">
+    <section class="dark:bg-dark-2 bg-white">
       <NavBar />
 
       <div class="mt-8 max-w-[570px] pl-8">
@@ -128,6 +130,10 @@ import { CURRENCY_CODES } from "../utils/constant";
 import AllPayments from "../components/transactions/AllPayments";
 import RegularPayments from "../components/transactions/RegularPayments";
 import VisaLogo from "../assets/visa.png";
+import VisaLogoLight from "../assets/visa-dark.png";
+import { useDark } from "@vueuse/core";
+import PlusPrimary from "../assets/plus-primary.png";
+import PlusSecondary from "../assets/plus-secondary.png";
 
 export default {
   name: "MyWallets",
@@ -142,6 +148,7 @@ export default {
     NavBar,
   },
   setup() {
+    const isDark = useDark();
     const accountSummary = ref({
       balance: 5240,
       percentageGain: "23.65%",
@@ -186,7 +193,11 @@ export default {
       accountSummary,
       toAmount,
       VisaLogo,
+      VisaLogoLight,
       CURRENCY_CODES,
+      isDark,
+      PlusPrimary,
+      PlusSecondary,
     };
   },
 };
