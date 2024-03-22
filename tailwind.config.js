e0407306e7e6b6ea7bb436c528b15e5287422123
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-03-22 06:53:43 +0100

    style: update styling config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
+        gordita: ['Gordita', 'sans-serif'],
       },
       colors: {
         primary: 'var(--primary-color)',
